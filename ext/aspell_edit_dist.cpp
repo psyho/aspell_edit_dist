fbb973f9c8f4c07d60980db97bef2723632b6263
Adam Pohorecki <adam.pohorecki@llp.pl>
2009-12-28 14:19:23 +0100

    implemented setters for the Aspell:EditDistanceWeights class

--- ext/aspell_edit_dist.cpp
+++ ext/aspell_edit_dist.cpp
@@ -38,28 +38,63 @@ static VALUE weights_del1(VALUE self) {
   return INT2FIX(get_weights(self)->del1);
 }
 
+static VALUE weights_set_del1(VALUE self, VALUE val) {
+  get_weights(self)->del1 = NUM2INT(val);
+  return val;
+}
+
 static VALUE weights_del2(VALUE self) {
   return INT2FIX(get_weights(self)->del2);
+}
+
+static VALUE weights_set_del2(VALUE self, VALUE val) {
+  get_weights(self)->del2 = NUM2INT(val);
+  return val;
 }
 
 static VALUE weights_swap(VALUE self) {
   return INT2FIX(get_weights(self)->swap);
 }
 
+static VALUE weights_set_swap(VALUE self, VALUE val) {
+  get_weights(self)->swap = NUM2INT(val);
+  return val;
+}
+
 static VALUE weights_sub(VALUE self) {
   return INT2FIX(get_weights(self)->sub);
+}
+
+static VALUE weights_set_sub(VALUE self, VALUE val) {
+  get_weights(self)->sub = NUM2INT(val);
+  return val;
 }
 
 static VALUE weights_similar(VALUE self) {
   return INT2FIX(get_weights(self)->similar);
 }
 
+static VALUE weights_set_similar(VALUE self, VALUE val) {
+  get_weights(self)->similar = NUM2INT(val);
+  return val;
+}
+
 static VALUE weights_min(VALUE self) {
   return INT2FIX(get_weights(self)->min);
 }
 
+static VALUE weights_set_min(VALUE self, VALUE val) {
+  get_weights(self)->min = NUM2INT(val);
+  return val;
+}
+
 static VALUE weights_max(VALUE self) {
   return INT2FIX(get_weights(self)->max);
+}
+
+static VALUE weights_set_max(VALUE self, VALUE val) {
+  get_weights(self)->max = NUM2INT(val);
+  return val;
 }
 
 typedef VALUE (*rb_method)(...);
@@ -73,12 +108,19 @@ void Init_edit_distance_weights() {
   rb_define_singleton_method(cEditDistanceWeights, "new", (rb_method)weights_init, 0);
 
   rb_define_method(cEditDistanceWeights, "del1",    (rb_method)weights_del1,     0);
+  rb_define_method(cEditDistanceWeights, "del1=",   (rb_method)weights_set_del1, 1);
   rb_define_method(cEditDistanceWeights, "del2",    (rb_method)weights_del2,     0);
+  rb_define_method(cEditDistanceWeights, "del2=",   (rb_method)weights_set_del2, 1);
   rb_define_method(cEditDistanceWeights, "swap",    (rb_method)weights_swap,     0);
+  rb_define_method(cEditDistanceWeights, "swap=",   (rb_method)weights_set_swap, 1);
   rb_define_method(cEditDistanceWeights, "sub",     (rb_method)weights_sub,      0);
+  rb_define_method(cEditDistanceWeights, "sub=",    (rb_method)weights_set_sub,  1);
   rb_define_method(cEditDistanceWeights, "similar", (rb_method)weights_similar,  0);
+  rb_define_method(cEditDistanceWeights, "similar=",(rb_method)weights_set_similar, 1);
   rb_define_method(cEditDistanceWeights, "min",     (rb_method)weights_min,      0);
+  rb_define_method(cEditDistanceWeights, "min=",    (rb_method)weights_set_min,  1);
   rb_define_method(cEditDistanceWeights, "max",     (rb_method)weights_max,      0);
+  rb_define_method(cEditDistanceWeights, "max=",    (rb_method)weights_set_max,  1);
 }
 
 void Init_limit_edit_distance() {
